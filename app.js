c3b7263c93e05d20ee8ef3937ceefcef5a91bc10
Kawish Zia <kawishzia2826@gmail.com>
2024-11-01 02:54:39 -0700

    assignment 2.js

---
+++ app.js
@@ -0,0 +1,49 @@
+
+
+// //-----------------------------------------------------------------------Assignment02(Variable for string)-----------------------------------------------
+// //----------------------------------------------------------------------Task No 01----------------------------------------------------------------------1. Declare a variable called username.
+// // 2. Declare a variable called myName & assign to it a string
+// // that represents your Full Name.
+// // 3. Write script to
+// // a) Declare a JS variable, titled message.
+// // b) Assign “Hello World” to variable message
+// // c) Display the message in alert box.
+// // 4. Write a script to save student’s bio data in JS variables and
+// // show the data in alert boxes
+// var username;
+// var myname = "kawish zia";
+// var message = "Hello World"
+// username = "kawish"
+// var age = "18";
+// var course = "Web and App"
+// alert(message);
+// alert(username );
+// alert(myname );
+// alert(age );
+// alert(course );
+
+
+// //----------------------------------------------------------------------Task No 02----------------------------------------------------------------------
+
+// // Write a script to display the following alert using one JS
+// // variable:
+
+// alert("PIZZA\nPIZZ\nPIZ\nPI\nP")
+
+// //----------------------------------------------------------------------Task No 03----------------------------------------------------------------------
+
+// // Declare a variable called email and assign to it a string that
+// // represents your Email Address(e.g. example@example.com).
+// // Show the blow mentioned message in an alert box.(Hint: use
+// // string concatenation)
+
+// var email  = "Kawihszia2826@gmail.com"
+// alert(email)
+// //----------------------------------------------------------------------Task No 04----------------------------------------------------------------------
+
+// // Declare a variable called book & give it the value “A
+// // smarter way to learn JavaScript”. Display the following
+// // message in an alert box
+// var book = "A smarter way to lern js";
+// alert("i am trying to lern from the " + book)
+
